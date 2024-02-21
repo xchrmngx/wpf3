67a69e55c2553da9d7c6ad16f5eb04488875ca04
Никита Ситников <nvidio960@mail.ru>
2024-02-21 21:18:53 +0500

    Добавьте файлы проекта.

---
+++ задание 2/Window5.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace задание_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window5.xaml
+    /// </summary>
+    public partial class Window5 : Window
+    {
+        public Window5()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Col1.SelectedIndex == 0)
+            {
+                BG.Background = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                txt1.Text = "Сейчас красный цвет";
+            }
+            else if (Col1.SelectedIndex == 1)
+            {
+                BG.Background = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+                txt1.Text = "Сейчас зеленый цвет";
+            }
+            else if (Col1.SelectedIndex == 2)
+            {
+                BG.Background = new SolidColorBrush(Color.FromRgb(0, 0, 255));
+                txt1.Text = "Сейчас синий цвет";
+            }
+                
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вычигин Тукай сделали эту программу, если что WPF гавно");
+        }
+    }
+}
---
+++ задание 2/Window6.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace задание_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window6.xaml
+    /// </summary>
+    public partial class Window6 : Window
+    {
+        public Window6()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Col1.SelectedIndex == 0)
+            {
+                Col2.Color = Color.FromRgb(255, 0, 0);
+            }
+            else if (Col1.SelectedIndex == 1)
+            {
+                Col2.Color = Color.FromRgb(0, 255, 0);
+            }
+            else if (Col1.SelectedIndex == 2)
+            {
+                Col2.Color = Color.FromRgb(0, 0, 255);
+            }
+            else if (Col1.SelectedIndex == 3)
+            {
+                Col2.Color = Color.FromRgb(255, 255, 255);
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Col2.Width = Slider1.Value;
+            Col2.Height = Slider1.Value;
+        }
+    }
+}
